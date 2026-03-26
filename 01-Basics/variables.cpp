64868ae3d7fca77c3c0a3a7ceeb74f7d0020e015
soumitrabiswas <soumitrabiswas2400@gmail.com>
2026-03-26 23:51:21 +0600

    Update variables.cpp

--- 01-Basics/variables.cpp
+++ 01-Basics/variables.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 int main()
 {
+    /*There are 2 steps
+    step 1 variable declaration*/
+    
     int num1,num2;
+    //step 2 value assign//
     num1=10;
     num2=20;
     int sum = num1 + num2;

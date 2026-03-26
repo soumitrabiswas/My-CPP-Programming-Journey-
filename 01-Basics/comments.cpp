44aac8188b33c4827b2cf2d1c5c3f5b00366a4bd
soumitrabiswas <soumitrabiswas2400@gmail.com>
2026-03-26 23:13:47 +0600

    Create comments.cpp

---
+++ 01-Basics/comments.cpp
@@ -0,0 +1,13 @@
+/*Today I
+learned
+about multiple line commnet*/
+#include <iostream>
+#include <conio.h>
+//My name here.It's single line comment
+using namespace std;
+int main()
+{
+    cout <<"\"This is your integer number :"<<endl;
+    getch();
+}
+
